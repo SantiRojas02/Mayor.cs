12abe000ada665f6f8ffa6d50f0d6960c7a6e53b
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-12 12:42:14 -0300

    Mayor.cs

---
+++ Mayor.cs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Mayor.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.Write("Ingrese valor1: ");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese valor2: ");
+            int valor1= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese valor3: ");
+            int valor2= Convert.ToInt32(Console.ReadLine());
+
+            if (valor > valor1 && valor > valor2)
+            {
+                Console.WriteLine("El numero mayor es: " +valor);
+            }
+            else
+            {
+                if (valor1 > valor && valor1 > valor2)
+                {
+                    Console.WriteLine("El numero mayor es: " + valor1);
+                }
+                else
+                {
+                    Console.WriteLine("El numero mayor es: " + valor2);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
